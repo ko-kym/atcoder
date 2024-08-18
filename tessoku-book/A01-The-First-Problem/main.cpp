ea67d79c76cdf597d40e547982367d1c724455cb
ko-kym <koutaroukym000@gmail.com>
2024-08-18 21:18:10 +0900

    feat: A01 - The First Problem

---
+++ tessoku-book/A01-The-First-Problem/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int inputNum;
+    cin >> inputNum;
+
+    int area = inputNum * inputNum;
+
+    cout << area << endl;
+
+    return 0;
+}
